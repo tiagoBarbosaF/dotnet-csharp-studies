d6231ab7f96c90db3aae20cd7f6f4743e088e778
Tiago Barbosa <tiago@farias.cc>
2021-08-28 14:56:26 -0300

    Using DotNet for Mac, basic tips and unit test

---
+++ Using DotNet for Mac/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:t}");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
