625b9f3852e7431447a10f7ebdd34fed83c14a4a
Tiago Barbosa <tiago@farias.cc>
2021-09-04 12:08:00 -0300

    Chat web with SignalR and TypeScript

---
+++ SignalRWebPack/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+using System.Threading.Tasks;
+
+namespace SignalRWebPack.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
