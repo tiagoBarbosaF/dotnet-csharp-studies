f431b1c03da352f426dbb30937405024f3e168a9
Tiago Barbosa <tiago@farias.cc>
2021-09-06 06:18:34 -0300

    Web Site for a fictional University, using Entity Framework Core tips and tricks - Part03

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+  public class OfficeAssignment
+  {
+    [Key] public int InstructorId { get; set; }
+
+    [StringLength(50), Display(Name = "Office Location")]
+    public string Location { get; set; }
+
+    public Instructor Instructor { get; set; }
+  }
+}
