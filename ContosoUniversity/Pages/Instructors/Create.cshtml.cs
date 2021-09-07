df6acb871c5a8dc002b3b64a6e5efae3f19a091f
Tiago Barbosa <tiago@farias.cc>
2021-09-06 22:21:34 -0300

    Web Site Contoso University - Adjustments: (Instructors section) -  Create, Edit, Delete and Index pages

--- ContosoUniversity/Pages/Instructors/Create.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Create.cshtml.cs
@@ -1,44 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class CreateModel : PageModel
+  public class CreateModel : InstructorCoursesPageModel
+  {
+    private readonly Data.SchoolContext _context;
+    private readonly ILogger<InstructorCoursesPageModel> _logger;
+
+    public CreateModel(Data.SchoolContext context, ILogger<InstructorCoursesPageModel> logger)
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
-
-        public CreateModel(ContosoUniversity.Data.SchoolContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
-
-        [BindProperty]
-        public Instructor Instructor { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Instructors.Add(Instructor);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+      _context = context;
+      _logger = logger;
     }
+
+    public IActionResult OnGet()
+    {
+      var instructor = new Instructor();
+      instructor.Courses = new List<Course>();
+
+      PopulateAssignedCourseData(_context, instructor);
+      return Page();
+    }
+
+    [BindProperty] public Instructor Instructor { get; set; }
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync(string[] selectedCourses)
+    {
+      var newInstructor = new Instructor();
+
+      if (selectedCourses.Length > 0)
+      {
+        newInstructor.Courses = new List<Course>();
+        _context.Courses.Load();
+      }
+
+      foreach (var course in selectedCourses)
+      {
+        var foundCourse = await _context.Courses.FindAsync(int.Parse(course));
+        if (foundCourse != null)
+          newInstructor.Courses.Add(foundCourse);
+        else
+          _logger.LogWarning($"Course {course} not found");
+      }
+
+      try
+      {
+        if (await TryUpdateModelAsync<Instructor>(
+          newInstructor,
+          "Instructor",
+          i => i.LastName,
+          i => i.FirstMidName,
+          i => i.HireDate,
+          i => i.OfficeAssignment))
+        {
+          _context.Instructors.Add(newInstructor);
+          await _context.SaveChangesAsync();
+          return RedirectToPage("./Index");
+        }
+      }
+      catch (Exception e)
+      {
+        _logger.LogError(e.Message);
+      }
+
+      PopulateAssignedCourseData(_context, newInstructor);
+      return Page();
+    }
+  }
 }
--- ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
@@ -1,59 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class DeleteModel : PageModel
+  public class DeleteModel : PageModel
+  {
+    private readonly Data.SchoolContext _context;
+
+    public DeleteModel(Data.SchoolContext context)
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
-
-        public DeleteModel(ContosoUniversity.Data.SchoolContext context)
-        {
-            _context = context;
-        }
-
-        [BindProperty]
-        public Instructor Instructor { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (Instructor == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Instructor = await _context.Instructors.FindAsync(id);
-
-            if (Instructor != null)
-            {
-                _context.Instructors.Remove(Instructor);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
-        }
+      _context = context;
     }
+
+    [BindProperty] public Instructor Instructor { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+      if (id == null)
+        return NotFound();
+
+      Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.Id == id);
+
+      if (Instructor == null)
+        return NotFound();
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+      if (id == null)
+        return NotFound();
+
+      Instructor instructor = await _context.Instructors
+        .Include(i => i.Courses)
+        .SingleAsync(i => i.Id == id);
+
+      if (instructor == null)
+        return RedirectToPage("./Index");
+
+      var departments = await _context.Departments
+        .Where(d => d.InstructorId == id)
+        .ToListAsync();
+      departments.ForEach(d => d.InstructorId = null);
+
+      _context.Instructors.Remove(instructor);
+
+      await _context.SaveChangesAsync();
+      return RedirectToPage("./Index");
+    }
+  }
 }
